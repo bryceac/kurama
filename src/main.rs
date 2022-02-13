b80beffa6e8ba23af601e989346a2d1182c9a360
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 10:37:25 -0800

    make sure site can deserialize itself.

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,10 @@ mod site;
 
 use serde_json;
 use crate::site::Site;
-use std::{fs::File, io::{ self, Read }};
+
 
 fn main() {
-    let site_configuration = match file_contents_from("config.json") {
-        Ok(content) => match serde_json::from_str::<Site>(&content) {
-            Ok(decoded_site) => decoded_site,
-            _ => Site::new()
-        },
-        _ => Site::new()
-    };
-
-    println!("{}", site_configuration.name)
+    println!("Hello, World!");
 }
 
-fn file_contents_from(f: &str) -> Result<String, io::Error> {
-    let mut file_content = String::new();
-    File::open(f)?.read_to_string(&mut file_content)?;
 
-    Ok(file_content)
-}
--- src/site.rs
+++ src/site.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize };
+use std::{fs::File, io::{ self, Read }};
 
 #[derive(Serialize, Deserialize)]
 pub struct Site {
@@ -19,4 +20,21 @@ impl Site {
             url: String::new()
         }
     }
+
+    pub fn from_file(f: &str) -> Result<Self, String> {
+        match file_contents_from("config.json") {
+            Ok(content) => match serde_json::from_str::<Site>(&content) {
+                Ok(decoded_site) => Ok(decoded_site),
+                Err(error) => Err(format!("{}", error))
+            },
+            Err(error) => Err(format!("{}", error))
+        }
+    }
+}
+
+fn file_contents_from(f: &str) -> Result<String, io::Error> {
+    let mut file_content = String::new();
+    File::open(f)?.read_to_string(&mut file_content)?;
+
+    Ok(file_content)
 }
