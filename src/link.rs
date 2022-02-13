c4ae025a8e95f8de318116d243c681a3473e2fe3
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 12:38:08 -0800

    add way to read links from file.

--- src/link.rs
+++ src/link.rs
@@ -5,3 +5,22 @@ pub struct Link {
     pub name: String,
     pub url: String
 }
+
+impl Link {
+    pub fn from_file(f: &str) -> Result<Self, String> {
+        match file_contents_from(f) {
+            Ok(content) => match serde_json::from_str::<Configuration>(&content) {
+                Ok(decoded_site) => Ok(decoded_site),
+                Err(error) => Err(format!("{}", error))
+            },
+            Err(error) => Err(format!("{}", error))
+        }
+    }
+}
+
+fn file_contents_from(f: &str) -> Result<String, io::Error> {
+    let mut file_content = String::new();
+    File::open(f)?.read_to_string(&mut file_content)?;
+
+    Ok(file_content)
+}
