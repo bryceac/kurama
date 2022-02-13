27ccdbbb07d1e9dc0e232d54d208173d62825fdf
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 11:29:26 -0800

    add more dependencies to get things working.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 mod configuration;
 
 use crate::configuration::Configuration;
+#[macro_use] extern crate lazy_static;
+use tera::{ Context, Tera };
 
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("templates/*.html") {
+            Ok(t) => t,
+            Err(error) => {
+                println!("Parsing error(s): {}", error);
+            }
+        };
+        tera
+    };
+}
 
 fn main() {
     let site_configuration = Configuration::from_file("config.json").expect("Could not load configuration");
+    let mut context = Context::new();
+    context.insert("site", &site_configuration);
 }
 
 
