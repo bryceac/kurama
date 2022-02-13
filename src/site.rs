c48791f7f929d05bf198bc4e931c4ad6d078e34c
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 10:26:54 -0800

    continue working on way to do site data.

--- src/site.rs
+++ src/site.rs
@@ -9,3 +9,14 @@ pub struct Site {
     pub dev_url: String,
     pub url: String
 }
+
+impl Site {
+    pub fn new() -> Self {
+        Site {
+            name: String::new(),
+            tagline: String::new(),
+            dev_url: String::new(),
+            url: String::new()
+        }
+    }
+}
