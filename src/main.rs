d99732170b8dbd50db09dad8ab46864840e69008
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 12:16:07 -0800

    take care of warnings.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use std::{ error::Error };
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        let mut tera = match Tera::new("templates/*.html") {
+        let tera = match Tera::new("templates/*.html") {
             Ok(t) => t,
             Err(error) => {
                 println!("Parsing error(s): {}", error);
