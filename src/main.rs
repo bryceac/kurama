f7cf437032d872755266b80f01b829bad2723f63
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 10:28:26 -0800

    create way to allow user to provide data.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ use crate::site::Site;
 use std::{fs::File, io::{ self, Read }};
 
 fn main() {
-    match file_contents_from("config.json") {
-        Ok(content) => match serde_json::from_str::Site(&content) {
-            Ok
-        }
-    }
+    let site_configuration = match file_contents_from("config.json") {
+        Ok(content) => match serde_json::from_str::<Site>(&content) {
+            Ok(decoded_site) => decoded_site,
+            _ => Site::new()
+        },
+        _ => Site::new()
+    };
+
+    println!("{}", site_configuration.name)
 }
 
 fn file_contents_from(f: &str) -> Result<String, io::Error> {
