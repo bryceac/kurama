eb1c3afecb20ac0b3e67f3a13b809faa8c03b4e7
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 09:49:46 -0800

    make properties optional JSON.

--- src/site.rs
+++ src/site.rs
@@ -3,7 +3,9 @@ use serde::{Serialize, Deserialize };
 #[derive(Serialize, Deserialize)]
 struct Site {
     name: String,
+    #[serde(default = "String::default")]
     tagline: String,
+    #[serde(default = "String::default")]
     dev_url: String,
     url: String
 }
