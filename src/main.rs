2889c6b47bbad8f3a18355e47c85e756d86535bc
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 20:48:18 -0800

    disable auto escape tera.

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,14 @@ use std::{fs::File, io::{ Write, Error } };
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
-        let tera = match Tera::new("templates/*.html") {
+        let mut tera = match Tera::new("templates/*.html") {
             Ok(t) => t,
             Err(error) => {
                 println!("Parsing error(s): {}", error);
                 ::std::process::exit(1);
             }
         };
+        tera.autoescape_on(vec![]);
         tera
     };
 }
