81f01ebad235d87d5522536f4701aa567edeaa79
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-13 19:02:06 -0800

    work on parsing front matter and content.

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,6 +1,12 @@
 use serde::{ Deserialize, Serialize };
 
-#[derive(Deserialize, Serialize )]
+#[derive(Deserialize, Serialize, Eq )]
 pub struct Metadata {
     pub title: String
+}
+
+impl PartialEq for Metadata {
+    fn eq(&self, other: &Self) -> bool {
+        self.title == other.title
+    }
 }
--- src/page.rs
+++ src/page.rs
@@ -1,9 +1,36 @@
 use serde::{ Deserialize, Serialize };
 use std::{fs::File, io::{ self, Read }};
 use crate::metadata::Metadata;
+use yaml_front_matter::YamlFrontMatter;
 
 #[derive(Deserialize, Serialize)]
 pub struct Page {
     pub metadata: Metadata,
     pub content: String
+}
+
+impl Page {
+    pub fn from_file(f: &str) -> Result<Self, String> {
+        match file_contents_from(f) {
+            Ok(content) => {
+                match YamlFrontMatter::parse::<Metadata>(&content) {
+                    Ok(document) => {
+                        Ok(Self {
+                            metadata: document.metadata,
+                            content: document.content
+                        })
+                    },
+                    Err(error) => format!("{}", error)
+                }
+            },
+            _ => Err(String::from("Could not parse file"))
+        }
+    }
+}
+
+fn file_contents_from(f: &str) -> Result<String, io::Error> {
+    let mut file_content = String::new();
+    File::open(f)?.read_to_string(&mut file_content)?;
+
+    Ok(file_content)
 }
