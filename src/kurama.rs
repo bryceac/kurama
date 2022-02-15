562b8dc92727b1f53b3d0bc16ca521f56263072b
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-15 14:58:58 -0800

    add version number to app.

--- src/kurama.rs
+++ src/kurama.rs
@@ -4,6 +4,7 @@ use crate::commands::Commands;
 #[derive(Parser)]
 #[clap(name = "kurama")]
 #[clap(about = "A Static site generator", long_about = None)]
+#[clap(version = "0.1")]
 pub struct Kurama {
     #[clap(subcommand)]
     pub command: Commands
