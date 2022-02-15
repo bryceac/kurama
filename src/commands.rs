f4b695ba7b1045b1101c188546f17dbce9d6fa27
Bryce Campbell <tonyhawk2100@gmail.com>
2022-02-15 15:07:54 -0800

    help user understand what the sub commands do.

--- src/commands.rs
+++ src/commands.rs
@@ -2,12 +2,17 @@ use clap::{ AppSettings, Subcommand };
 
 #[derive(Subcommand)]
 pub enum Commands {
+    #[clap(about = "deletes the output directory", long_about = None)]
     Clean {},
+    #[clap(about = "create a project directory at the given path", long_about = None)]
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     Create {
         path: String
     },
+    #[clap(about = "build the website", long_about = None)]
     Generate {},
+    #[clap(about = "create project directory structure in current directory", long_about = None)]
     Init {},
+    #[clap(about = "start dev server, to preview a website", long_about = None)]
     Serve {}
 }
