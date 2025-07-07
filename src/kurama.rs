2d9bfa67a44c66c094c6446298ce4197e501f0d9
Bryce Campbell <tonyhawk2100@gmail.com>
2025-07-06 22:50:16 -0700

    update version number in source.

--- src/kurama.rs
+++ src/kurama.rs
@@ -4,7 +4,7 @@ use crate::commands::Commands;
 #[derive(Parser)]
 #[clap(about = "A Static site generator", long_about = None,
 author = "Bryce Campbell <tonyhawk2100@gmail.com>",
-version = "0.1.1")]
+version = "0.2.0")]
 pub struct Kurama {
     #[clap(subcommand)]
     pub command: Commands
