2815eac01d9794f821e16026bb640d54fa560678
Bryce Campbell <tonyhawk2100@gmail.com>
2025-07-06 20:38:50 -0700

    work on reducing things down to necessities.

--- src/kurama.rs
+++ src/kurama.rs
@@ -2,10 +2,9 @@ use clap::{ Parser };
 use crate::commands::Commands;
 
 #[derive(Parser)]
-#[clap(name = "kurama")]
-#[clap(about = "A Static site generator", long_about = None)]
-#[clap(author = "Bryce Campbell <tonyhawk2100@gmail.com>")]
-#[clap(version = "0.1.1")]
+#[clap(about = "A Static site generator", long_about = None,
+author = "Bryce Campbell <tonyhawk2100@gmail.com>",
+version = "0.1.1")]
 pub struct Kurama {
     #[clap(subcommand)]
     pub command: Commands
