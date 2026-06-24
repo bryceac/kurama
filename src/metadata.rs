3377a412c99cf905cfccdeadd5b1a8943ae867ce
Bryce Campbell <tonyhawk2100@gmail.com>
2026-06-24 16:35:11 -0700

    add date field to metadata.

--- src/metadata.rs
+++ src/metadata.rs
@@ -1,15 +1,18 @@
+use chrono::NaiveDate;
 use serde::{ Deserialize, Serialize };
 
 #[derive(Deserialize, Serialize, Eq )]
 pub struct Metadata {
     #[serde(default="String::default")]
     pub title: String,
+    pub date: Option<NaiveDate>,
     pub slug: String
 }
 
 impl PartialEq for Metadata {
     fn eq(&self, other: &Self) -> bool {
         self.title == other.title &&
+        self.date == other.date &&
         self.slug == other.slug
     }
 }
