2753c98dc30a753b4608908bb99caaaf53fe358f
Bryce Campbell <tonyhawk2100@gmail.com>
2026-07-01 15:03:20 -0700

    make sure user dir is parsed.

--- src/create.rs
+++ src/create.rs
@@ -16,7 +16,11 @@ impl Create {
     }
 
     pub async fn run(&self) {
-        let site_path = Path::new(&self.path);
+        let site_path = Path::new(if self.path.starts_with("~") {
+            shellexpand::tilde(&self.path).into_owned()
+        } self {
+            self.path
+        });
     
         if !Path::exists(site_path) {
            if let Err(error) = create_dir_all(site_path) {
