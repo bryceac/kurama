6198dca652d846aa49a863ee5070e74ca3dea446
Bryce Campbell <tonyhawk2100@gmail.com>
2026-07-07 17:19:25 -0700

    migrate date components to metadata.

--- src/metadata.rs
+++ src/metadata.rs
@@ -9,6 +9,24 @@ pub struct Metadata {
     pub slug: String
 }
 
+impl Metadata {
+    pub fn date_components(&self) -> Vec<String> {
+        if let Some(date) = self.date {
+            let components: Vec<String> = date.to_rfc3339()
+                .split("T")
+                .map(|c| c.to_owned())
+                .collect();
+
+            components[0].split("-")
+            .map(|c| c.to_owned())
+            .collect()
+        } else {
+            vec![]
+        }
+        
+    }
+}
+
 impl PartialEq for Metadata {
     fn eq(&self, other: &Self) -> bool {
         self.title == other.title &&
