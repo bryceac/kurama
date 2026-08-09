4127aec0c5940b18e5d9d8f518bfdde24311ef06
Bryce Campbell <tonyhawk2100@gmail.com>
2026-08-09 09:01:58 -0700

    finish implementing basic taxonomy.

---
+++ src/taxonomy.rs
@@ -0,0 +1,50 @@
+use std::fmt::Display;
+use serde::{ Serialize, Deserialize };
+use rslug::Slugifier;
+
+pub trait Taxonomy {
+    fn slug(&self) -> String;
+}
+
+pub struct Tag {
+    pub name: String
+}
+
+impl Tag {
+    pub fn from(s: &str) -> Self {
+        Self { name: s.to_owned() }
+    }
+}
+
+impl Display for Tag {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+impl Taxonomy for Tag {
+    fn slug(&self) -> String {
+        let slugify_with_underscore = Slugifier::new()
+        .separator("_");
+
+        slugify_with_underscore.slugify(&self.name)
+    }
+}
+
+impl Serialize for Tag {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: serde::Serializer {
+        
+        serializer.serialize_str(&self.name)
+    }
+}
+
+impl<'de> Deserialize<'de> for Tag {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+            D: serde::Deserializer<'de> {
+        let value = String::deserialize(deserializer)?;
+        Ok(Self::from(&value))
+    }
+}
